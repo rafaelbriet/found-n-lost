320d2071f1e45d305ebab43dfd6a31f9f22126b0
Rafael Briet <rafael-briet@hotmail.com>
2022-02-22 19:00:36 -0300

    Player is able to use spray

--- Assets/__Project/Scripts/Character.cs
+++ Assets/__Project/Scripts/Character.cs
@@ -7,7 +7,10 @@ public class Character : MonoBehaviour
     [SerializeField]
     private int maxHitPoints = 100;
 
+    private Coroutine sprayCoroutine;
+
     public int CurrentHitPoints { get; private set; }
+    public bool HasSprayApplied { get; private set; }
 
     private void Awake()
     {
@@ -17,5 +20,28 @@ public class Character : MonoBehaviour
     public void Damage(int amout)
     {
         CurrentHitPoints -= amout;
+    }
+
+    public void ApplySpray(float duration)
+    {
+        if (sprayCoroutine != null)
+        {
+            StopCoroutine(sprayCoroutine);
+        }
+
+        sprayCoroutine = StartCoroutine(ApplySprayCoroutine(duration));
+    }
+
+    private IEnumerator ApplySprayCoroutine(float duration)
+    {
+        HasSprayApplied = true;
+
+        while (duration > 0)
+        {
+            duration -= Time.deltaTime;
+            yield return null;
+        }
+
+        HasSprayApplied = false;
     }
 }
---
+++ Assets/__Project/Scripts/Spray.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewSpray", menuName = "Itens/Spray")]
+public class Spray : Item
+{
+    [SerializeField]
+    private float durationInSeconds = 15f;
+    [SerializeField]
+    private float range = 5f;
+    [SerializeField]
+    private float areaOfEffect = 1f;
+    [SerializeField]
+    private LayerMask layerMask;
+
+    public override void Use(ItemUseOptions options)
+    {
+        Vector3 mouseWorldPosition = options.Camera.ScreenToWorldPoint(options.MousePosition);
+        
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(mouseWorldPosition, areaOfEffect, layerMask);
+
+        foreach (Collider2D collider in colliders)
+        {
+            if (Vector3.Distance(options.Owner.transform.position, collider.transform.position) < range)
+            {
+                collider.GetComponent<Character>().ApplySpray(durationInSeconds);
+            }
+        }
+    }
+}
