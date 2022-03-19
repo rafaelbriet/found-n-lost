345acc6322fd0b10a7bc35a3bae33ff2a95b7bb4
Rafael Briet <rafael-briet@hotmail.com>
2022-03-19 00:13:13 -0300

    Add credits screen to the main menu

--- Assets/__Project/Scripts/MainMenu.cs
+++ Assets/__Project/Scripts/MainMenu.cs
@@ -5,8 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject creditsCanvas;
+
+    private void Awake()
+    {
+        creditsCanvas.SetActive(false);
+    }
+
     public void Play()
     {
         SceneManager.LoadScene("Gameplay");
+    }
+
+    public void ToggleCreditsCanvas()
+    {
+        creditsCanvas.SetActive(!creditsCanvas.activeInHierarchy);
     }
 }
